462fbbb83039a4599f25dbd192a65053efcc7708
amira2566 <braemira2@icloud.com>
2018-07-09 02:20:09 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,20 @@
 
 # Code your methods below
+def dance_instructions
+  puts "plant legs far apart, bend knees slightly and keep posture loose"
+  puts "lift right foot"
+  puts "return right foot to the ground"
+  puts "finishing with a small skip-step backward"
+  puts "lower left foot to the ground"
+  puts "bounce left foot back up slightly, kicking it a few inches back"
+  puts "right, left, right and right"
+  puts "holding your arms out in front of you, straight and at chest level"
+  puts "cross your right wrist over your left and hold them together"
+  puts "lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times"
+  puts "start arm movement by holding left arm so the back of your wrist is near your chin, pointing your elbow straight left"
+  puts "lift right arm up and point right elbow diagonally to the right"
+  puts "raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a rope lasso. This movement is repeated eight times as well"
+  puts "assume stance"
+  puts "bust a move"
+  
+  
